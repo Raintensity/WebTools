f29efafb4d4013a1193e49ac18e7badb3b2f0cfa
Raintensity <dev@usx.jp>
2025-01-02 20:27:26 +0900

    Update global header (collapse/expand)

--- src/components/parts/header.tsx
+++ src/components/parts/header.tsx
@@ -1,29 +1,55 @@
 import { PreactDOMAttributes } from "preact";
+import { useState } from "preact/hooks";
 import { AppLink } from "components/tags/applink";
+
+const stateStorageKey = "global:header-state";
 
 interface GlobalHeaderProps extends PreactDOMAttributes {
 	isForceMinimal?: boolean
 	isCentering?: boolean
 }
+export const GlobalHeader = (props: GlobalHeaderProps) => {
+	const storageState = window.localStorage.getItem(stateStorageKey) ? true : false;
+	const [initialState, setInitialState] = useState(props.isForceMinimal);
+	const [isMinimal, setMinimal] = useState(storageState);
 
-const Full = () => (
+	if (initialState !== props.isForceMinimal) {
+		setInitialState(props.isForceMinimal);
+		setMinimal(props.isForceMinimal ? true : storageState);
+	}
+
+	const changeState = (e?: Event) => {
+		e?.preventDefault();
+		setMinimal(prev => {
+			if (props.isForceMinimal) return !prev;
+			if (prev) {
+				window.localStorage.removeItem(stateStorageKey);
+			} else {
+				window.localStorage.setItem(stateStorageKey, "1")
+			}
+			return !prev;
+		});
+	};
+
+	if (isMinimal) {
+		return <Minimal changeState={changeState} />;
+	}
+	return <Full changeState={changeState} />;
+};
+
+interface HeaderProps extends PreactDOMAttributes {
+	changeState: () => void
+}
+const Full = (props: HeaderProps) => (
 	<div id="global-header">
 		<ul>
 			<li><AppLink><h2>WebTools</h2></AppLink></li>
 			<li style="flex:1"></li>
 			<li><a href="https://github.com/Raintensity/WebTools" target="_blank" rel="noopener noreferrer"><img src="/assets/img/common/github.svg" alt="GitHub" width="20" height="20" /></a></li>
-			<li>✕</li>
+			<li><a href="#" onClick={props.changeState}>&#x23f5;</a></li>
 		</ul>
 	</div>
 );
-
-const Minimal = () => (
-	<div id="global-header-minimal">MinimalHeader</div>
+const Minimal = (props: HeaderProps) => (
+	<div id="global-header-minimal"><p><a href="#" onClick={props.changeState}>&#x23f4;</a></p></div>
 );
-
-export const GlobalHeader = (props: GlobalHeaderProps) => {
-	if (props.isForceMinimal) {
-		return <Minimal />;
-	}
-	return <Full />;
-};
